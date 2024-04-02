f1fada9694f8a5e5eb8f7862ffb48527e01c221b
yangdonglai <yangdl1995@163.com>
2024-04-02 16:59:52 +0800

    fn run use Result<>

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
-use std::{env, fs, process};
+use std::{env, error::Error, fs, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    run(&config)
+    // let _ = run(&config);
+    if let Err(e ) = run(&config){
+        println!("Application error: {e}");
+        process::exit(1)
+    }
     // println!("Searching for {} in {}", config.query, config.file_path);
 }
 
-fn run(config: &Config) {
+fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
-    fs::read_to_string(config.file_path).expect("should have been able to read the file");
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("With text:\n{contents}");
+
+    Ok(())
 }
 
 struct Config<'a> {
